ed14806cdbddf3a512b74c9046a15b6788d7cd0c
Monsieur19 <42003367+Monsieur19@users.noreply.github.com>
2019-05-06 15:49:48 +0300

    fix 2.1

--- gulpfile.js
+++ gulpfile.js
@@ -91,6 +91,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/img/sprite-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
